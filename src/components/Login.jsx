735c5d9301126d80855b4dab65f7c3ba7d69c28c
rimpa kasundi <rimpakasundi@gmail.com>
2025-10-18 20:56:07 +0530

    Add Firebase Google authentication login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,24 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import React from "react";
+import { auth } from "../firebase/firebase.init";
+
+const googleProvider = new GoogleAuthProvider();
 
 const Login = () => {
+  const handleGoogleSignIn = () => {
+    // console.log("Google button clicked");
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div>
       <h2>PLease login</h2>
+      <button onClick={handleGoogleSignIn}>Sign in with Google</button>
     </div>
   );
 };
